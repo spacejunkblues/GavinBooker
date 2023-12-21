386c934c752eefc45d096c2b7ff558ba2e663e03
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2023-12-21 06:54:53 -0600

    change metrics landing page to profile

--- metrics/urls.py
+++ metrics/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.metric_view, name='Metrics'),
-    path('<int:year>/<int:month>', views.metric_view, name='Metrics'),
+    path('', views.profilestatus_view, name='Metrics'),
+    path('visits/<int:year>/<int:month>', views.visits_view, name='Metrics'),
     path('gigs/<int:year>/<int:month>', views.gigs_view, name='Gigs'),
     path('profile/<int:year>/<int:month>', views.profilestatus_view, name='Profile'),
     path('problem', views.report_problem, name='Problem'),
